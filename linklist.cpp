1dddad137cd07c25f9b58b387d2644a1d4b13c66
Din4m <410691529@qq.com>
2020-04-29 22:01:47 +0800

    链表

---
+++ linklist.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct node{
+    float v=0;
+    node *next=NULL;
+    /* data */
+};
+
+int main(){
+    vector<float> f({1.0,2.0,3.0});
+    node l;
+    node *head=&l;
+    for(auto it:f){
+        node tmpl;
+        head->v=it;
+        head->next=new node;
+        head = head.next;
+        // head.v=it;
+        // head.next=new node;
+        // head      =head.next;
+    }
+    while(l.next!=NULL){
+        cout<<l.v;
+        l=*(l.next);
+    }
+}
