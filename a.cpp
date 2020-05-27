931369d0713b45b209b1c2ed0d39f5e27ac9fe5a
Din4m <410691529@qq.com>
2020-05-27 22:12:41 +0800

    update

--- a.cpp
+++ a.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct node{
+    long data;
+    node *next=nullptr;
+};
+struct znode{
+    int data;
+    vector<int> nei;    
+};
+znode *networks;
+vector<node> v;
 int main(){
     int c;
-    a;
+    cout<<sizeof(v)<<endl;
+    cout<<sizeof(v[0])<<endl;
+    cout<<sizeof(v.capacity())<<endl;
+    //a;//error
 }
