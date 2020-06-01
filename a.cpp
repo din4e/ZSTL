eeba4ee8b1974d8ade15a1452917f2ef6902b43e
Din4m <410691529@qq.com>
2020-06-01 22:39:56 +0800

    updata

--- a.cpp
+++ a.cpp
@@ -5,15 +5,18 @@ struct node{
     node *next=nullptr;
 };
 struct znode{
-    int data;
-    vector<int> nei;    
+    int data; //4
+    vector<int> nei; //28
 };
-znode *networks;
+znode networks;
 vector<node> v;
 int main(){
     int c;
     cout<<sizeof(v)<<endl;
     cout<<sizeof(v[0])<<endl;
     cout<<sizeof(v.capacity())<<endl;
+    //cout<<sizeof(*networks)<<endl;
+    cout<<sizeof(networks.nei)<<endl;
+    cout<<networks.nei.capacity()<<endl;
     //a;//error
 }
