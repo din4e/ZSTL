4231b9099412ae47d30881d65993f23516d4d000
Din4m <410691529@qq.com>
2020-07-15 22:37:16 +0800

    update ptr.cpp/string_char.cpp

---
+++ string_char.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char s1[]="hello";
+    char s2[]="hello";
+    char *s3="hello";
+    char *s4="hello";
+    if(s1==s2) cout<<"s1==s2\n";
+    else cout<<"s1!=s2\n";
+    if(s3==s4) cout<<"s3==s4\n";
+    else cout<<"s3!=s4\n";
+    return 0;
+}
