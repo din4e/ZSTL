a1412cecf3aba1291395e73f00a04d062fdbcb43
Din4m <410691529@qq.com>
2020-07-26 23:02:08 +0800

    Revert "update test.cpp"
    This reverts commit 1c4901c92ed864347feb71fcd8b5a22b1292afa7.

--- tcp_ip/test.cpp
+++ tcp_ip/test.cpp
@@ -3,12 +3,6 @@
 #include<pthread.h>
 using namespace std;
 int main(){
-    //cout<<"FINISHED.";
-    vector<int> v(11);
-    for(int i=0;i<12;i++){
-        v.push_back(i);
-        cout<<v.capacity()<<"\n";
-    }
-    
+    cout<<"FINISHED.";
     return 0;
 }
