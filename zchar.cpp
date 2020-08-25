064616f352518673e78aba2819d1c3d23c73aa69
Din4m <410691529@qq.com>
2020-08-26 00:06:58 +0800

    Update zchar.cpp

--- zchar.cpp
+++ zchar.cpp
@@ -1,12 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct zchar
-{
-    /* data */
-};
-
-int main(){
+char * zstrcpy(char *strDest, const char *strSrc){
+    assert((strDest!=NULL)&&strSrc!=NULL);// assert 
+    char *address=strDest;
+    while((*strDest++ =* strSrc++) !='\0' );
+    return address;
+}
+void test_zstrcpy(){
+    char c[10]="sdas";
+    char s[10]="";
+    cout<<zstrcpy(s,c)<<"\n";
+}
+void test_minus(){
+    char x[4]="123";
     char y[8]="2131231";
     cout<<y<<"\n";
-    cout<<y[-3]; // y[-1]
+    cout<<y[-1]<<"\n"; // y[-1]
+}
+int main(){
+    test_zstrcpy();
+    test_minus();
+    return 0;
 }
