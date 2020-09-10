cf655558d91d6c47d5cba70a96dcc3bd3e2211e8
Din4m <410691529@qq.com>
2020-09-10 23:16:48 +0800

    add gdb.cpp

--- copy.cpp
+++ copy.cpp
@@ -3,11 +3,12 @@ using namespace std;
 // struct内部持有一个vector,拷贝struct实例时候会发生深拷贝还是浅拷贝? map呢? 都是深拷贝 vector自身就是完全杜绝浅拷贝
 struct A{
     vector<int>  v; // 24 8*3
-    map<int,int> m; 
-};
+    map<int,int> m; // 48
+}; // 72 
 int main(){
     A a,b;
     b=a;
+    cout<<sizeof(A)<<" "<<sizeof(a.m)<<"\n";
     cout<<&a<<" "<<&(a.v)<<" "<<&(a.m)<<"\n";
     cout<<&b<<" "<<&(b.v)<<" "<<&(b.m)<<"\n";
     // 0xffffcbb0 0xffffcbb0 0xffffcbc8 
---
+++ gdb.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void func(int i){
+    cout<<i<<'\n';
+}
+int main(int argc, char *argv[])
+{
+    for(int i=0;i<5;++i){
+        func(i);
+    }
+    return 0;
+}
